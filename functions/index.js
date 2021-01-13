75a26269c79d5fe401187fc9186bb4121a87d79f
Cody Snell <codyjsnell@yahoo.com>
2021-01-13 14:23:13 -0600

    validates all signup fields

--- functions/index.js
+++ functions/index.js
@@ -57,6 +57,17 @@ app.post("/shout", (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+
 // signup route
 app.post("/signup", (req, res) => {
   const newUser = {
@@ -66,6 +77,20 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
 
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Email is required";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Password is required";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Handle is required";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
   // TODO validate data
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
